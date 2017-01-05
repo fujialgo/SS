4863a6b0c6e2e2f07c4c3b4548dc690ca74da805
shana <roisixyana@gmail.com>
2017-01-06 00:40:21 +0900

    Player

---
+++ SacredSpear/Assets/Scene/Game/Player/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    public int m_speed = 10;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * m_speed;
+    }
+}
---
+++ SacredSpear/Assets/Scene/Game/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bullet;
+
+    const float m_speed = 0.01f;
+
+    void Update()
+    {
+       
+        Ray m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 m_mousePos = m_ray.origin;
+
+        Vector3 m_diff = m_mousePos - transform.position;
+        Vector3 m_norm = m_diff.normalized;
+
+        // マウスの方向を向かせる
+        float deg = Mathf.Atan2(m_diff.y, m_diff.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, deg - 90);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
