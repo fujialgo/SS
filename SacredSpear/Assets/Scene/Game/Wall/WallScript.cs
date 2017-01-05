472f038a1bb8082178011a5a25e2493b7ecaacdb
kaneko <iwgm14cyancle@gmail.com>
2017-01-06 00:08:02 +0900

    適当に地面置いた

--- SacredSpear/Assets/Scene/Game/Wall/WallScript.cs
+++ SacredSpear/Assets/Scene/Game/Wall/WallScript.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class WallScript : MonoBehaviour {
 
+
+    [SerializeField]
+    private int m_hp;
+    public int mHp
+    {
+        get { return m_hp; }
+        set { m_hp = value; }
+    }
+    
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
+    void OnGUI()
+    {
+        GUILayout.Label(m_hp.ToString());
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.Space))
+        {
+            --m_hp;
+        }
+
+        if (m_hp <= 0)
+        {
+            DestroyObject(gameObject);
+        }
 	}
 }
