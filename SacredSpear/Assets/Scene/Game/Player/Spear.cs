6519c65c2d54628aa820c8b5bb523bb8f5298ec8
SHANA\a <roisixyana@gmail.com>
2017-01-18 10:03:18 +0900

    スピア

---
+++ SacredSpear/Assets/Scene/Game/Player/Spear.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Spear: MonoBehaviour
+{
+
+    public int m_speed = 10;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * m_speed *Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            other.gameObject.GetComponent<EnemyMove>().mDeath();
+        }
+    }
+}
