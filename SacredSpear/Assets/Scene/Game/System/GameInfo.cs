6ce9c5d019b2450d07c51f4043af6d7169c78e6b
kaneko <iwgm14cyancle@gmail.com>
2017-01-18 11:21:59 +0900

    GameInfoと各プレハブ更新

---
+++ SacredSpear/Assets/Scene/Game/System/GameInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInfo : MonoBehaviour {
+    [SerializeField]
+    Point m_point;
+    public Point mPoint
+    {
+        get { return m_point; }
+    }
+
+}
